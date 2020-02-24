8455c9501792755e554fbaf08d31b0b782d030b1
Trevil1990 <48261569+Trevil1990@users.noreply.github.com>
2020-02-24 02:05:45 +0200

    Add Collection for 3.2

---
+++ Collection/Collection.php
@@ -0,0 +1,50 @@
+<?php
+
+class Collection 
+{
+    private $storage = array();
+
+    //Adds an object inside the  storage
+    public function add($key, $data = null) {
+	    if ($data == null) {
+	        $this->storage[] = $key;
+	    } else { $this->storage[$data] = $key;	        
+	   	 }
+	}
+
+	//Removes the object from the storage
+    public function remove($key) {
+	    if (isset($this->storage[$key])) {
+	        unset($this->storage[$key]);
+	    }
+	}
+
+	//This method calculates an identifier for the objects. You should use any hash function
+	public function getHash($key) {
+	    return spl_object_hash($this->storage[$key]);
+	}
+
+	//Return last added object.
+	public function current() {
+	    return end($this->storage);
+	}
+
+	//Return list of all added objects.
+	public function getObjectList() {
+	    return $this->storage;
+	}
+
+	//Function should return true if object exist in the collection, false - if not
+	public function check($key) {
+	    if (isset($this->storage[$key])) {
+	        echo 'true';
+	    } else {
+	    	echo 'false';
+	    }
+	}
+
+	//Static method. Should remove all objects from collection
+ //    public function removeAll($key) {
+	//    return unset($this->storage);
+	// }
+}
---
+++ Collection/index.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once "Collection.php";
+
+class Storage
+{
+    private $name;
+    private $number;
+
+    public function __construct($name, $number) {
+        $this->name = $name;
+        $this->number = $number;
+    }
+
+    public function __toString() {
+        return $this->name . " is number " . $this->number;
+    }
+}
+
+$new = new Collection();
+$new->add(new Storage("Max", 14));
+$new->add(new Storage("Lora", 37));
+$new->add(new Storage("Alexey", 49));
+$new->add(new Storage("Anton", 50), "anton");
+$new->add(new Storage("Marina", 51));
+
+$new->remove("0");
+
+echo '<pre>';
+print_r($new->getObjectList());
+echo 'Alexey now is '.($new->getHash(2));
+echo '</pre>';
