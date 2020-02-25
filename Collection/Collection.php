300fad812ecfa6086a91c800502bc53beb77ccf3
Trevil1990 <48261569+Trevil1990@users.noreply.github.com>
2020-02-25 12:25:59 +0200

    collection commit hash

--- Collection/Collection.php
+++ Collection/Collection.php
@@ -1,50 +1,67 @@
 <?php
 
+/**
+ * TE comment:
+ * You need to use getHash in add, remove, check functions
+ * example: $this->storage[$this->getHash($key)] = $data
+ */
 class Collection 
 {
-    private $storage = array();
+    private $storage = [];
 
     //Adds an object inside the  storage
-    public function add($key, $data = null) {
+    public function add($key, $data = null)
+    {
 	    if ($data == null) {
 	        $this->storage[] = $key;
-	    } else { $this->storage[$data] = $key;	        
+	    } else { $this->storage[$this->generationHash($data)] = $key;	        
 	   	 }
 	}
 
 	//Removes the object from the storage
-    public function remove($key) {
-	    if (isset($this->storage[$key])) {
-	        unset($this->storage[$key]);
+    public function remove($key)
+    {
+	    if (isset($this->storage[$this->getHash($key)])) {
+	        unset($this->storage[$this->getHash($key)]);
 	    }
 	}
 
 	//This method calculates an identifier for the objects. You should use any hash function
-	public function getHash($key) {
-	    return spl_object_hash($this->storage[$key]);
-	}
+	 public function getHash($key): string 
+	 {
+	  return $this->generationHash($key);
+	 }
+
+	 public function generationHash($key): string 
+	 {
+	  return md5($key);
+	 }
+
+	// public function getHash($key, $algorithm = 'md5') {
+	// 	return hash($algorithm, $this->storage[$key]);
+	// }
 
 	//Return last added object.
-	public function current() {
+	public function current(): object 
+	{
 	    return end($this->storage);
 	}
 
 	//Return list of all added objects.
-	public function getObjectList() {
+	public function getObjectList(): array 
+	{
 	    return $this->storage;
 	}
 
 	//Function should return true if object exist in the collection, false - if not
-	public function check($key) {
-	    if (isset($this->storage[$key])) {
-	        echo 'true';
-	    } else {
-	    	echo 'false';
-	    }
+	public function check($key): boolean
+	{
+		return isset($this->storage[$this->getHash($key)]) ? true : false;
 	}
 
 	//Static method. Should remove all objects from collection
-	public function removeAll($key) {
+	public function removeAll($key)
+	{
 		unset($this->storage);
 	}
 }
--- Collection/index.php
+++ Collection/index.php
@@ -1,32 +1,35 @@
 <?php
 
-require_once "Collection/Collection.php";
+require_once "Collection.php";
 
 class Storage
 {
     private $name;
     private $number;
 
-    public function __construct($name, $number) {
+    public function __construct($name, $number)
+    {
         $this->name = $name;
         $this->number = $number;
     }
 
-    public function __toString() {
+    public function __toString()
+    {
         return $this->name . " is number " . $this->number;
     }
 }
 
 $new = new Collection();
-$new->add(new Storage("Max", 14));
-$new->add(new Storage("Lora", 37));
-$new->add(new Storage("Alexey", 49));
+$new->add(new Storage("Max", 14), 'max');
+$new->add(new Storage("Lora", 37), 'lora');
+$new->add(new Storage("Alexey", 49), 'alexey');
 $new->add(new Storage("Anton", 50), "anton");
-$new->add(new Storage("Marina", 51));
-
-$new->remove("0");
+$new->add(new Storage("Marina", 51), 'marina');
+$new->add(new Storage("Anna", 52), 'Anna');
+var_dump($new);
+$new->remove("max");
 
 echo '<pre>';
 print_r($new->getObjectList());
-echo 'Alexey now is '.($new->getHash(2));
+echo $new->getHash(3);
 echo '</pre>';
